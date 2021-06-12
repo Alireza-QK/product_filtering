e97a8174ad8fd2aa9d082a7a18af9467c96cfdf3
Alireza-QK <Alireza.QK.75@gmail.com>
2021-06-12 13:03:30 +0430

    create form view and add to urls path

--- store/product/urls.py
+++ store/product/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from .views import create_product
 
 app_name = 'product'
 
 urlpatterns = [
-    
+    path('create/', create_product, name='create'),
 ]
--- store/product/views.py
+++ store/product/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from .models import Product, Category
+from .forms import ProductCreateForm
 
 
 def create_product(request):
 
     if request.method == 'POST':
+        product_form = ProductCreateForm(request.POST)
         pass
+    else:
+        product_form = ProductCreateForm()
     
-    context = {}
+    context = {
+        'form': product_form
+    }
     return render(request, 'product/create.html', context)
